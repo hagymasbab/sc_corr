b77911f470bc2ab1f8fdd615a045819c4775438d
Mihály Bányai <banyai.mihaly@wigner.mta.hu>
2016-04-01 00:29:43 +0200

    bias test results

--- cmm_test_biases.py
+++ cmm_test_biases.py
@@ -1,30 +1,60 @@
 import numpy as np
 from corrmeasure import correlationMeasurementModel as cMM
+import pickle
+import matplotlib.pyplot as plt
 
 
 def histogramMode(data, resolution):
-    return 1
+    binVals, binEdges = np.histogram(data, bins=resolution)
+    maxIdx = binVals.argmax()
+    return (binEdges[maxIdx] + binEdges[maxIdx+1]) / 2
 
-cmm = cMM('corr_rate.pkl')
+recalc = False
 
 n_unit = 2
 n_bin = 1
 n_trial = 100
 
-n_corrs = 2
-n_means = 2
-n_reest = 2
+n_corrs = 10
+n_means = 4
+n_reest = 5
 
-est_vs_true = np.zeros((n_means, n_corrs, n_reest, 2))
+mean_vals = np.linspace(0.5, 3, n_means)
+print(mean_vals)
 
-for mp_mean in np.linspace(0, 3, n_means):
-    for mp_corr in np.linspace(-0.9, 0.9, n_corrs):
-        mu_mp = mp_mean * np.ones(n_unit)
-        var_mp = 1 * np.ones(n_unit)
-        corr_mp = np.array([[1, mp_corr], [mp_corr, 1]])
+corr_vals = np.linspace(-0.9, 0.9, n_corrs)
 
-        for i in range(n_reest):
-            sc_synth = cmm.generate(corr_mp, mu_mp, var_mp, n_trial, n_bin)
-            mp_samples = cmm.infer(sc_synth[0], sc_synth[1], sc_synth[2], n_trial, n_bin, 100, 200, 1)
-            MAP_corr = histogramMode(mp_samples[0][:, 0, 1], 20)
-            print(MAP_corr)
+if recalc:
+    cmm = cMM('corr_rate.pkl')
+
+    est_vs_true = np.zeros((n_means, n_corrs, n_reest, 2))
+
+    for m in range(n_means):
+        for c in range(n_corrs):
+            mu_mp = mean_vals[m] * np.ones(n_unit)
+            var_mp = 1 * np.ones(n_unit)
+            corr_mp = np.array([[1, corr_vals[c]], [corr_vals[c], 1]])
+
+            for i in range(n_reest):
+                print(i)
+                sc_corr, sc_mean, sc_var = cmm.generate(corr_mp, mu_mp, var_mp, n_trial, n_bin)
+                mps_corr, mps_mean, mps_var = cmm.infer(sc_corr, sc_mean, sc_var, n_trial, n_bin, 100, 1000, 1)
+                MAP_corr = histogramMode(mps_corr[:, 0, 1], 20)
+                est_vs_true[m, c, i, :] = [corr_vals[c], MAP_corr]
+
+    pickle.dump(est_vs_true, open('biastest.pkl', 'wb'))
+else:
+    est_vs_true = pickle.load(open('biastest.pkl', 'rb'))
+
+for m in range(n_means):
+    plt.subplot(1, n_means, m+1)
+    plt.scatter(est_vs_true[m, :, :, 0], est_vs_true[m, :, :, 1])
+    plt.xlim([-1, 1])
+    plt.ylim([-1, 1])
+    plt.plot([-1, 1], [-1, 1], color="black", linestyle='--', linewidth=1)
+    plt.title("mean=%.2f" % mean_vals[m])
+    if m == 0:
+        plt.ylabel("posterior MAP of MP corr")
+    if m == np.floor(n_means / 2) - 1:
+        plt.xlabel("true MP corr")
+plt.show()
--- corrmeasure.py
+++ corrmeasure.py
@@ -31,7 +31,7 @@ class correlationMeasurementModel:
         sc_mean = np.mean(spike_count, axis=0)
         sc_var = np.var(spike_count, axis=0)
         sc_corr = np.corrcoef(spike_count.T)
-        return (sc_corr, sc_mean, sc_var)
+        return sc_corr, sc_mean, sc_var
 
     def infer(self, sc_corr, sc_mean, sc_var, n_trial, n_bin, n_samp_est, n_iter, n_chains):
         n_unit = len(sc_mean)
@@ -56,6 +56,6 @@ class correlationMeasurementModel:
             'stdnorm_samples': stdnorm_samples
         }
 
-        fit = self.sm.sampling(data=corr_dat, iter=n_iter, chains=n_chains)
+        fit = self.sm.sampling(data=corr_dat, iter=n_iter, chains=n_chains, refresh=0)
         estimation = fit.extract(permuted=True)
-        return (estimation['mp_corr_mat'], estimation['mp_mean_vec'], estimation['mp_var_vec'])
+        return estimation['mp_corr_mat'], estimation['mp_mean_vec'], estimation['mp_var_vec']
